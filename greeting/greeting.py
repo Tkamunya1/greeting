4c7f3658450a84cda33ddaba6ab074796f14973a
Titus <titus.kamunya@student.moringaschool.com>
2023-06-30 16:52:34 +0300

    first commit

---
+++ greeting/greeting.py
@@ -0,0 +1,7 @@
+def say_hello(name):
+    """Prints a greeting message."""
+    print(f"Hello, {name}!")
+
+def say_goodbye(name):
+    """Prints a goodbye message."""
+    print(f"Goodbye, {name}!")
