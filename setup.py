35ea7317cc63cc4d64acab849aa4267c04a9c454
Titus <titus.kamunya@student.moringaschool.com>
2023-06-30 16:56:13 +0300

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='greeting',
+    version='1.0.0',
+    packages=['greeting'],
+    description='A simple package for greetings',
+    author='Titus K',
+    author_email='tituskamunya.com',
+    url='https://github.com/Tkamunya1/greeting',
+)
